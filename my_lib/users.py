c6846b7e21befcf8309e20cec27d1e94d3ca3d24
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-02-28 11:50:40 +0100

    feat: Adds basic python library

---
+++ my_lib/users.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2023, Quack AI.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+from typing import List
+
+__all__ = ["greet_contributor"]
+
+
+def greet_contributor(name: str) -> str:
+	"""Creates a string message to greet the contributor.
+
+	Args:
+		name: name of the person to greet
+
+	Returns:
+		the greeting message
+	"""
+	return f"Hello {name}! Nice to meet you."
