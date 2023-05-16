52242b288632fcca1c88059980ded2e8d3a356a7
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-05-16 17:43:03 +0200

    feat: Adds a table processing function for users

--- my_lib/users.py
+++ my_lib/users.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-__all__ = ["greet_contributor"]
+__all__ = ["greet_contributor", "process_table"]
 
 
 def greet_contributor(name: str) -> str:
@@ -16,3 +16,33 @@ def greet_contributor(name: str) -> str:
         the greeting message
     """
     return f"Hello {name}! Nice to meet you."
+
+
+def validate_user_info(user_info: Dict[str, Any]) -> bool:
+    """Validate each payload to avoid errors later in backend
+
+    Args:
+        user_info: a dictionary with at least 3 entries (created_at, updated_at, name)
+    Returns:
+        whether the payload can be processed
+    """
+    # Check datetime
+    if user_info["created_at"] > user_info["last_updated"] or user_info["last_updated"] > datetime.utcnow().isoformat():
+        return False
+    # Check identity
+    if len(user_info["name"]) < 5:
+        return False
+
+    return True
+
+
+def process_table(users: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Process the SQL table to only keep the ones that aren't corrupted
+
+    Args:
+        users: list where each element is a user entry from the table
+    Returns:
+        the list of processable user payloads
+    """
+    valid_users = [user for user in users if validate_user_info(user)]
+    return valid_users
