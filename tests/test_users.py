9581b3b291d7665838bdc389c902a94efb6cb87c
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-05-16 18:31:06 +0200

    test: Fixes formatting on tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 import pytest
+
 from my_lib.users import validate_user_info
 
 
@@ -6,9 +7,23 @@ from my_lib.users import validate_user_info
     "user_info, is_valid",
     [
         [{"name": "Richard Hendricks"}, False],
-        [{"created": "2023-05-15T14:42:47.571141", "updated_at": "2023-05-15T15:24:27.648219", "name": "Richard Hendricks"}, False],
-        [{"created_at": "2023-05-15T14:42:47.571141", "updated_at": "2023-05-15T15:24:27.648219", "name": "Richard Hendricks"}, True],
+        [
+            {
+                "created": "2023-05-15T14:42:47.571141",
+                "updated_at": "2023-05-15T15:24:27.648219",
+                "name": "Richard Hendricks",
+            },
+            False,
+        ],
+        [
+            {
+                "created_at": "2023-05-15T14:42:47.571141",
+                "updated_at": "2023-05-15T15:24:27.648219",
+                "name": "Richard Hendricks",
+            },
+            True,
+        ],
     ],
 )
 def test_validate_user_info(user_info, is_valid):
-	assert validate_user_info(user_info) is is_valid
+    assert validate_user_info(user_info) is is_valid
